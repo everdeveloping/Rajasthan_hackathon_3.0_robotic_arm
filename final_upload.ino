a3797a0916d1474503c6deab5d7b0ea9d8ed8d33
Kishan Srinivasan <32857906+everdeveloping@users.noreply.github.com>
2017-12-03 03:25:07 +0530

    Add files via upload

---
+++ final_upload.ino
@@ -0,0 +1,96 @@
+#include <Servo.h>
+
+Servo servomotor1, servomotor2;
+Servo servo4;
+Servo servo5;
+Servo servo6;
+int y, x;
+int pos, pos1;
+const int xpin = A5;                  // x-axis of the accelerometer
+const int ypin = A4;                  // y-axis
+const int zpin = A3;                  // z-axis (only on 3-axis models)
+const int flexpin1 = A0;
+const int flexpin2 = A1;
+const int flexpin3 = A2;
+
+void setup() {
+  // initialize the serial communications:
+  Serial.begin(9600);
+  servomotor2.attach(9);
+  servomotor1.attach(10);
+  servo4.attach(4);
+  servo5.attach(5);
+  servo6.attach(6);
+  pos = 90;
+  pos1 = 90;
+  servomotor2.write(90);
+  servomotor1.write(90);
+  servo4.write(90);
+  servo5.write(90);
+  servo6.write(90);
+  delay(100);
+
+}
+
+void loop() {
+  int flexposition1;
+  int flexposition2;
+  int flexposition3;
+  int servoposition4;
+  int servoposition5;
+  int servoposition6;
+  y = analogRead(ypin);
+  x = analogRead(xpin);
+  if (y < 300)
+  { pos = pos + 2;
+    if (pos >= 180)
+      pos = 180;
+    delay(15);
+  }
+
+  if (y > 340)
+  {  pos = pos - 2;
+    if (pos <= 0)
+      pos = 0;
+    delay(15);
+  }
+
+  int angle = servomotor2.read();
+  Serial.println(angle);
+
+  //servomotor2.write(pos);
+
+  //delay(50);
+
+  if (x < 285)
+  { pos1 = pos1 + 2;
+    if (pos1 >= 180)
+      pos1 = 180;
+    delay(15);
+  }
+
+  if (x > 335)
+  { pos1 = pos1 - 2;
+    if (pos1 <= 0)
+      pos1 = 0;
+    delay(15);
+  }
+
+  int angle1 = servomotor1.read();
+  Serial.println(angle1);
+  flexposition1 = analogRead(flexpin1);
+  flexposition2 = analogRead(flexpin2);
+  flexposition3 = analogRead(flexpin3);
+  servoposition4 = map(flexposition1, 922, 850, 90, 45);
+  //servoposition4 = constrain(servoposition4, 0, 180);
+  servoposition5 = map(flexposition2, 922, 850, 90, 45);
+  //servoposition5 = constrain(servoposition5, 0, 180);
+  servoposition6 = map(flexposition3, 922, 850, 90, 135);
+
+  servomotor2.write(pos);
+  servomotor1.write(pos1);
+  servo4.write(servoposition4);
+  servo5.write(servoposition5);
+  servo6.write(servoposition6);
+  delay(50);
+}
---
+++ up1.1_test.ino
@@ -0,0 +1,104 @@
+#include <Servo.h> 
+
+// Create a servo "object", called servo1. Each servo object
+// controls one servo (you can have a maximum of 12):
+
+Servo servo4;
+Servo servo5;
+Servo servo6;//int servoposition=0;
+// Define the analog input pin to measure flex sensor position:
+
+const int flexpin1 = 0; 
+const int flexpin2 = 0;
+const int flexpin3 = 0;
+void setup() 
+{ 
+  // Use the serial monitor window to help debug our sketch:
+
+  Serial.begin(9600);
+
+  // Enable control of a servo on pin 9:
+
+  servo4.attach(3);
+  servo5.attach(5);
+  servo6.attach(6);
+  servo4.write(90);
+  servo5.write(90);
+  servo6.write(90);
+  
+} 
+void loop()
+{
+
+}
+/*
+void loop() 
+{ 
+  int flexposition1;
+  int flexposition2;
+  int flexposition3;// Input value from the analog pin.
+  int servoposition4;
+  int servoposition5;
+  int servoposition6;// Output value to the servo.
+
+  // Read the position of the flex sensor (0 to 1023):
+
+  flexposition1 = analogRead(flexpin1);
+  flexposition2 = analogRead(flexpin2);
+  flexposition3 = analogRead(flexpin3);
+  // Because the voltage divider circuit only returns a portion
+  // of the 0-1023 range of analogRead(), we'll map() that range
+  // to the servo's range of 0 to 180 degrees. The flex sensors
+  // we use are usually in the 600-900 range:
+
+  servoposition4 = map(flexposition1, 62, 82, 0, 180);
+  servoposition4 = constrain(servoposition4, 0, 180);
+  servoposition5 = map(flexposition2, 62, 82, 0, 180);
+  servoposition5 = constrain(servoposition5, 0, 180);
+  servoposition6 = map(flexposition3, 62, 82, 0, 180);
+  servoposition6 = constrain(servoposition6, 0, 180);
+  // if(flexposition<82)
+   //{
+    //servoposition=servoposition+2;
+    //if(servoposition>=180)
+    //servoposition=180;
+    //delay(15);
+    
+   //}
+     // if(flexposition>85)
+   //{
+    //servoposition=servoposition-2;
+    //if(servoposition<=0)
+    //servoposition=0;
+    //delay(15);
+    
+   //}
+  // Now we'll command the servo to move to that position:
+
+  servo4.write(servoposition4);
+  servo5.write(servoposition5);
+  servo6.write(servoposition6);
+  // Because every flex sensor has a slightly different resistance,
+  // the 600-900 range may not exactly cover the flex sensor's
+  // output. To help tune our program, we'll use the serial port to
+  // print out our values to the serial monitor window:
+
+  Serial.print("sensor: ");
+  Serial.print(flexposition1 "\t" felxposition2 "\t" flexposition3"\t");
+  Serial.print("  servo4: ");
+  Serial.println(servoposition4 "\t" servoposition5 "\t" servoposition6 "\n");
+  delay(250);
+  // Note that all of the above lines are "print" except for the
+  // last line which is "println". This puts everything on the
+  // same line, then sends a final carriage return to move to
+  // the next line.
+
+  // After you upload the sketch, turn on the serial monitor
+  // (the magnifying-glass icon to the right of the icon bar).
+  // You'll be able to see the sensor values. Bend the flex sensor
+  // and note its minimum and maximum values. If you replace the
+  // 600 and 900 in the map() function above, you'll exactly match
+  // the flex sensor's range with the servo's range.
+
+  //delay(100);  // wait 20ms between servo updates
+} */
